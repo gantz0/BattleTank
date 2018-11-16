c4c9cdefd85adb18309e493cb7c91f795534cd0f
jmcrew <jmloversg@gmail.com>
2018-11-16 11:28:32 +0900

    first commit project

---
+++ Project/Source/unrealbattletank/unrealbattletankGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "unrealbattletankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALBATTLETANK_API AunrealbattletankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
